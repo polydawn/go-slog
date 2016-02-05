666e00167dd3f7618d57f6f1097ea57d1212fd8f
Eric Myhre <hash@exultant.us>
2016-02-04 16:24:23 -0800

    Docs!
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- slog.go
+++ slog.go
@@ -32,6 +32,12 @@ type Slog struct {
 	opine func(io.Writer)
 }
 
+/*
+	Produce a new `Slog`.  All of the output will be written to the provided
+	`io.Writer` (often `os.Stderr` is a reasonable choice for this).  Every
+	time the status area needs to be updated, `statFunc` is called, and is
+	provided another writer to use.
+*/
 func New(proxy io.Writer, statFunc func(io.Writer)) *Slog {
 	return &Slog{
 		wr:    proxy,
@@ -39,6 +45,14 @@ func New(proxy io.Writer, statFunc func(io.Writer)) *Slog {
 	}
 }
 
+/*
+	Write a message to the scrollback -- it will appear just above the
+	updating status area.
+
+	This implements the `io.Writer` contract.  Proxy other loggers to this!
+	Then you get your choice of logging framework, AND the ability to do
+	interactively updating status info with Slog.
+*/
 func (slog *Slog) Write(msg []byte) (int, error) {
 	slog.retract()             // bring the cursor back up
 	n, e := slog.wr.Write(msg) // write the message
@@ -46,11 +60,33 @@ func (slog *Slog) Write(msg []byte) (int, error) {
 	return n, e                // return stats from the parameter's write
 }
 
+/*
+	Update the status area.
+
+	Calls the `statFunc` the Slog was initialized with, and the content it
+	produces will replace the current status area.
+
+	If you have a bunch of progress bars, they might all feed status events
+	into one list of progress info -- call this whenever the list is updated.
+	(You're free to do dedup of events or trigger this on a timer, whatever.)
+*/
 func (slog *Slog) Refresh() {
 	slog.retract()
 	slog.place()
 }
 
+/*
+	Set the current status area adrift in the scrollback.  It will no longer
+	be overwritten after you call `Drape()` -- as you continue to write
+	other logs, the draped status text will just drift up in the scrollback
+	like any other lines.  New status writes after `Drape` behave normally
+	(they'll still auto-replace themselves).
+
+	You might want to clear out your status text immediatley after calling
+	`Drape()`; otherwise you'll get the whole text duplicated in the scrollback,
+	and again below it as soon as you next call either `Refresh` or `Write`.
+	Of course, you might want that, so the choice is yours.
+*/
 func (slog *Slog) Drape() {
 	slog.mls.footprint = 0
 }
